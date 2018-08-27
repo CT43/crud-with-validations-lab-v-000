5a97d48ac53ce3b6e16dee2bce1dd9882cefc0ae
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-27 21:42:28 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-   validates :title, uniqueness: {
+  validates :title, uniqueness: {
      scope: %i[release_year artist_name],
      message: 'cannot be repeated by the same artist in the same year'
    }
