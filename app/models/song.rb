a8b553a220c95823259411e3f3b6df6c796a6711
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-27 21:42:52 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, uniqueness: {
-     scope: %i[release_year artist_name]
-   }
+  validates :title, uniqueness: {scope: %i[release_year artist_name]}
    validates :released, inclusion: { in: [true, false] }
    validates :artist_name, presence: true
 
